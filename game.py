bb0ebdfc6cf28445e578588ed62e84bb844c9048
Thomas <twatson52@mac.com>
2012-02-20 07:57:33 -0500

    added a class for loading a tileset

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ import settings
 class Game: #class for our game engine
 	def __init__(self, g):
 		self.g = g #store global variables
-		self.surf = pygame.Surface((settings.screen_x*settings.screen_scale, \
-			settings.screen_y*settings.screen_scale)) #create a new surface to display on
+		self.surf = pygame.Surface((settings.screen_x, settings.screen_y)) #create a new surface to display on
+		self.surf.convert() #convert it to the display format for faster blitting
 		self.surf.fill((255, 255, 255)) #fill it white for now
 	def start(self):
 		pass
---
+++ tileset.py
@@ -0,0 +1,26 @@
+import pygame #import all of pygame
+from pygame.locals import *
+
+#class for managing a tileset
+class Tileset:
+	#initialize the tileset
+	def __init__(self, image, tile_width, tile_height, trans=None):
+		self.image = pygame.image.load(image) #load the image provided
+		if trans is not None: #if there is a transparent pixel color
+			self.image.set_colorkey(trans) #set it as the color key
+		self.image.convert() #convert it to display format for faster blitting
+		self.tile_width = tile_width #save attributes
+		self.tile_height = tile_height
+		self.tiles_x = self.image.get_width()/tile_width #store number of tiles
+		self.tiles_y = self.image.get_height()/tile_height
+	#function to get a specific tile
+	def get_tile(self, x, y=None, dest=None):
+		if y is None: #if a y value was not specified
+			y = x/self.tiles_x #calculate it
+			x = x-y #and update x properly
+		if dest is None: #if no destination surface was specified
+			dest = pygame.Surface((self.tile_width, self.tile_height)) #create a new one
+			dest.convert() #and convert it for faster drawing
+		dest.blit(self.image, (0, 0), pygame.Rect(x*self.tile_width, y*self.tile_height, \
+			self.tile_width, self.tile_height)) #draw the specified tile to the destination
+		return dest #and return it
