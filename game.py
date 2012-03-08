ada2b22008e06945c1dba7f62b1fb420f107cabb
Thomas <twatson52@mac.com>
2012-03-08 14:06:33 -0500

    only show fps in debug mode

--- game.py
+++ game.py
@@ -162,5 +162,5 @@ class Game: #class for our game engine
 				pos = self.dialog_talking.pos
 				pos = (pos[0]-self.camera_pos[0]+12, pos[1]-self.camera_pos[1]+8)
 				pygame.draw.polygon(self.surf, (255, 255, 255), [[35, 46], pos, [65, 46]])
-		self.font.render(str(self.g.clock.get_fps()).split(".")[0], self.surf, (0, 180)) #draw framerate
+		if self.debug: self.font.render(str(self.g.clock.get_fps()).split(".")[0], self.surf, (0, 180)) #draw framerate
 		return self.surf #return the rendered surface
