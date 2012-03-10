518c23609378062d4f55cc0283bbf616753e627d
Thomas <twatson52@mac.com>
2012-03-10 09:46:40 -0500

    game now uses transitions for warping

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ import objects #and objects
 import font #font manager
 import player #class for player
 import dialog #class for dialogs
+import transition #import all the transitions
 
 class Game: #class for our game engine
 	def __init__(self, g):
@@ -17,11 +18,11 @@ class Game: #class for our game engine
 		self.camera_pos = [368, 336] #set default camera position
 		self.objects = {} #list of objects on the map
 		self.warps = {} #list of warps on the map
-		self.warping = 0 #set when we need to do a warp
 		self.warp_obj = None #warp object
+		self.curr_transition = None #current transition object
+		self.transition_cb = None #callback when transition completes
 		self.obj2pos = {} #dictionary of objects mapped to positions
 		self.pos2obj = {} #dictionary of positions mapped to objects
-		self.overlay_color = None
 		self.default_dialog = dialog.Dialog(self.g, "standard")
 		self.dialog = None
 		self.font = self.default_dialog.dlog_font
@@ -33,7 +34,8 @@ class Game: #class for our game engine
 	def start(self):
 		self.player = player.Player(self) #initialize a player object
 		self.load_map("data/maps/oasis.tmx") #load map
-		self.warping = 2
+		self.map_image = self.map.update() #update it once
+		self.transition(transition.FadeIn(32)) #start fade in
 	def load_map(self, map_file): #load a map
 		self.map = map.Map(self.g, map_file) #load the map
 		objects_dom = parse(self.map.properties["object_data"]).documentElement #parse object data file
@@ -57,14 +59,14 @@ class Game: #class for our game engine
 	def add_warp(self, pos, obj): #add a warp object
 		self.warps[pos] = obj #store the warp
 	def prepare_warp(self, pos): #prepare a warp
-		self.warping = True #we're about to warp
 		self.warp_obj = self.warps[pos] #get the warp object
+		#start transition
+		self.transition(transition.FadeOut(32), callback=self.perform_warp)
 	def perform_warp(self): #actually warp
 		warp_obj = self.warp_obj #get the warp objectx
 		self.objects = {} #destroy map objects
 		self.warps = {}
 		self.map = None
-		self.warping = 2 #we're in warp stage 2
 		self.warp_obj = False
 		self.obj2pos = {}
 		self.pos2obj = {}
@@ -78,7 +80,8 @@ class Game: #class for our game engine
 		player.tile_pos = new_pos #set player position
 		player.pos = [((player.tile_pos[0]-1)*16)+8, (player.tile_pos[1]-1)*16]
 		player.rect = pygame.Rect(player.pos, player.size)
-		self.overlay_color = None
+		self.map_image = self.map.update() #update map once
+		self.transition(transition.FadeIn(32)) #start fade back in
 	def get_tile_type(self, tile_x, tile_y, player_req=False): #get the type of tile at the given position
 		if tile_y < 0 or tile_x < 0: #if the tile is negative
 			return -1 #it shouldn't exist
@@ -117,39 +120,31 @@ class Game: #class for our game engine
 	def interact(self, pos, direction): #interact with an object
 		if pos in self.pos2obj: #if this position has an object
 			self.pos2obj[pos].interact(direction) #tell the object to interact
+	def transition(self, obj, callback=None): #start a transition
+		self.curr_transition = obj #store transition object
+		self.transition_cb = callback #and callback
 	def update(self): #update the engine for this frame
-		if self.warping == 1: #if we should warp
-			if self.overlay_color is None: #if we just started
-				self.overlay_color = [255, 0, 0, 0] #start color
-			else:
-				self.overlay_color[0] -= 32 #decrement color
-			self.overlay_color = [self.overlay_color[0]]*3
-			if self.overlay_color[0] <= 0: #if we're at full black
-				self.perform_warp() #do the warp
-		if self.warping == 2: #if we're fading back in
-			if self.overlay_color is None: #if we just started
-				self.overlay_color = [0, 0 ,0, 0] #start color
-			else:
-				self.overlay_color[0] += 32 #increment color
-			self.overlay_color = [self.overlay_color[0]]*3
-			if self.overlay_color[0] > 255: #if we're at max brightness
-				self.overlay_color = None #stop fading
-				self.warping = 0
 		if self.g.curr_keys[settings.key_debug]: #if the debug key is pressed
 			self.debug = not self.debug #invert debug flag
 		#center camera on player
 		pos = self.objects["player"].pos #get position of player
 		self.camera_pos = (pos[0]-(settings.screen_x/2)+16, pos[1]-(settings.screen_y/2)+16)
-		if self.warping != 1:
+		if self.curr_transition is None: #if there is no transition going on now
 			self.map_image = self.map.update() #update the map
 		self.surf.fill((0, 0, 0)) #clear surface for black background
 		if self.debug: #if we're in debug mode
 			for pos in self.pos2obj: #draw object collision tiles
 				self.map_image.fill((255, 0, 0), rect=pygame.Rect(((pos[0]*16, pos[1]*16), (16, 16))), special_flags=BLEND_RGB_MULT)
+		#draw map
 		self.surf.blit(self.map_image, (0, 0), pygame.Rect(self.camera_pos, \
 			(settings.screen_x, settings.screen_y))) #blit it
-		if self.overlay_color is not None: #if there's a color to render over the surface
-			self.surf.fill(self.overlay_color, special_flags=BLEND_RGB_MULT) #do so
+		if self.curr_transition is not None: #if there is a transition happening
+			r = self.curr_transition.update(self.surf) #update it
+			if r: #if it finished
+				self.curr_transition = None #destroy transition object
+				if self.transition_cb is not None: #if there is a callback
+					self.transition_cb() #call it			
+					self.transition_cb = None #destroy callback
 		if self.dialog_drawing: #if we're drawing a dialog
 			result = self.dialog.update(self.surf, (0, 1)) #draw it
 			if result is not None: #if we're finished
