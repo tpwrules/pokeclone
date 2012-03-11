9fe4ec7eaed9d06ad6c8ff2d82a4d9834fdb59df
Thomas <twatson52@mac.com>
2012-03-11 07:55:56 -0400

    added if command to script

--- script.py
+++ script.py
@@ -21,11 +21,43 @@ class Script:
 			if n.nodeType == n.TEXT_NODE: #if it's a text node
 				nodes.append(n.data) #add it to the list of text
 		return "".join(nodes) #return combined string
+	def get_var(self, var): #parse a variable string and return its value
+		try: #try to parse it as a variable
+			type = var[0] #get parts
+			name = var[1:]
+			if type == ".": #if it's a local variable
+				return self.vars[name] #return its value
+		except:
+			pass
+		try: #try to parse it as a number
+			return int(var)
+		except:
+			pass
+		return None #we couldn't parse it
 	def dialog_cb(self, result): #callback for dialog completion
 		self.vars["dlog_result"] = result #store result in variables
 	def cmd_dialog(self, cmd): #handle command
 		self.dlog_wait = True #we're waiting for a dialog
 		self.obj.game.show_dlog(self.get_text(cmd), self.obj, callback=self.dialog_cb) #show the dialog
+	def cmd_if(self, cmd): #handle if command
+		#get parameters
+		left = self.get_var(cmd.getAttribute("left"))
+		op = cmd.getAttribute("op")
+		right = self.get_var(cmd.getAttribute("right"))
+		comparison = False #result of the comparison
+		if op == "=": #if we're comparing equality
+			comparison = True if left == right else False #do comparison
+		if comparison: #if the comparison was true
+			next = cmd.getElementsByTagName("then")[0] #get the then element
+			self.callstack.append(self.curr_command.nextSibling) #save next command
+			self.curr_command = next.firstChild #store first then command
+		else: #if the comparison failed
+			next = cmd.getElementsByTagName("else") #get an else element
+			if len(next) == 0: #if there is none
+				self.curr_command = self.curr_command.nextSibling #go to next command
+				return
+			self.callstack.append(self.curr_command.nextSibling) #save next command
+			self.curr_command = next[0].firstChild #store first command of else
 	def next_cmd(self): #process the next command
 		if not self.running: return True #return if we aren't running
 		#return if we're waiting for a dialog and one is being shown
@@ -38,6 +70,9 @@ class Script:
 				self.curr_command = self.callstack.pop() #get another command from the callstack
 		if self.curr_command.localName == "dialog": #if it's a dialog command
 			self.cmd_dialog(self.curr_command) #handle it
+		elif self.curr_command.localName == "if": #if it's an if
+			self.cmd_if(self.curr_command) #handle it
+			return #skip going to next command
 		self.curr_command = self.curr_command.nextSibling #go to next command
 	def update(self): #update script state
 		if not self.running: return #return if we aren't running
