e8f41d54bb4e064b0a8dce9c74651f3f88549fae
Thomas <twatson52@mac.com>
2012-03-18 06:15:24 -0400

    added definition for a cancel key

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ screen_x = 256 #size of screen before scaling
 screen_y = 192
 framerate = 30 #framerate to keep
 
-keys = [-1, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT, ord("x"), ord("p")] #array of keys
+keys = [-1, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT, ord("x"), ord("p"), ord("z")] #array of keys
 
 key_escape = 1 #index into keys array
 key_up = 2
@@ -14,6 +14,7 @@ key_left = 4
 key_right = 5
 key_accept = 6
 key_debug = 7
+key_cancel = 8
 
 TILE_NORMAL = 0
 TILE_SOLID = 1
