78ed6ae5220427254482b8dcb71e657883db55e0
Thomas <twatson52@mac.com>
2012-04-04 11:12:27 -0400

    created new classes for part animation

--- animation.py
+++ animation.py
@@ -83,3 +83,118 @@ class AnimationGroup:
 	#update the animation
 	def update(self):
 		self.curr_animation.update() #update the current animation
+
+class PartAnimationPart: #class for one part in the layout
+	def __init__(self, set_, g, dom): #create a part based on an element
+		self.g = g #store given parameters
+		self.set = set_
+
+		self.id = dom.getAttribute("id") #get id of this part
+		set_.parts[id] = self #store ourselves
+		self.pos = [int(x.strip()) for x in dom.getAttribute("pos").split(",")] #and position
+		try: #load rotation
+			self.rot = int(dom.getAttribute("rotation"))
+		except:
+			self.rot = 0
+		try: #load scale
+			t = float(dom.getAttribute("scale"))
+		except:
+			t = 1.0
+		self.xscale, self.yscale = t, t
+		try:
+			self.xscale = float(dom.getAttribute("xscale"))
+		except:
+			pass
+		try:
+			self.yscale = float(dom.getAttribute("yscale"))
+		except:
+			pass
+		self.image = set_.part_images[dom.getAttribute("from")] #load our image
+	def render(surf, x, y, xs, ys, rot): #render ourselves
+		img = self.image #get starting surface
+		if rot+self.rot != 0: #if we need to rotate
+			img = pygame.transform.rotate(img, rot+self.rot) #do so
+		if xs*self.xscale != 1.0 or ys*self.yscale != 1.0: #if we need to scale
+			size = (img.get_width()*xs*self.xscale, img.get_height()*ys*self.yscale) #calculate new size
+			img = pygame.transform.smoothscale(img, size) #do the scale
+		surf.blit(img, (x+self.pos[0], y+self.pos[1])) #draw transformed image
+
+class PartAnimationGroup: #class for a layout group in a part animation
+	def __init__(self, set_, g, dom): #create a group based on a dom
+		self.g = g #store given parameters
+		self.set = set_
+
+		self.id = dom.getAttribute("id") #get id of this group
+		set_.parts[id] = self #store ourselves
+		self.pos = [int(x.strip()) for x in dom.getAttribute("pos").split(",")] #and position
+		try: #load rotation
+			self.rot = int(dom.getAttribute("rotation"))
+		except:
+			self.rot = 0
+		try: #load scale
+			t = float(dom.getAttribute("scale"))
+		except:
+			t = 1.0
+		self.xscale, self.yscale = t, t
+		try:
+			self.xscale = float(dom.getAttribute("xscale"))
+		except:
+			pass
+		try:
+			self.yscale = float(dom.getAttribute("yscale"))
+		except:
+			pass
+		#load children
+		node = dom.firstChild
+		self.children = []
+		while node is not None:
+			if node.localName == "group": #create new group if necessary
+				self.children.append(PartAnimationGroup(set_, g, node))
+			elif node.localName == "part": #or part
+				self.children.append(PartAnimationPart(set_, g, node))
+			node = node.nextSibling
+	def render(self, surf, x, y, xs, ys, rot): #render ourselves
+		for child in self.children: #render each child
+			child.render(surf, x+self.pos[0], y+self.pos[1], xs*self.xscale, ys*self.yscale, rot+self.rot)
+
+#set of part animations from one file
+class PartAnimationSet:
+	def __init__(self, g, anim_file):
+		self.g = g #store given parameters
+
+		images = {} #dictionary of images loaded
+		self.part_images = {} #dictionary of part images defined
+		self.parts = {} #dictionary of different parts defined
+		self.layout = None #the part layout
+
+		#load data from given file
+		anim_dom = parse(anim_file).documentElement
+		#load all given images
+		for image in anim_dom.getElementsByTagName("image"):
+			f = image.getAttribute("from") #get source file so we can load it
+			id = image.getAttribute("id") #and image id
+			surf = pygame.image.load(f) #load given image
+			surf.convert_alpha() #convert it for faster rendering
+			images[id] = surf #store it
+		#define all the part images
+		for part_image in anim_dom.getElementsByTagName("part_image"):
+			image = images[part_image.getAttribute("from")] #get image used
+			#get source coordinate
+			coord = [int(x.strip()) for x in part_image.getAttribute("coord").split(",")]
+			if part_image.getAttribute("center") == "": #if no center was defined
+				surf = pygame.Surface((coord[2], coord[3]), SRCALPHA) #create a surface for the image
+				surf.blit(image, (0, 0), coord) #draw image onto new surface normally
+			else: #if one was defined, we need to shift the image around
+				#get center coordinate
+				center = [int(x.strip()) for x in part_image.getAttribute("center").split(",")]
+				#calculate the difference in current center and wanted center
+				center_diff = ((coord[2]/2)-center[0], (coord[3]/2)-center[1])
+				#calculate new size of surface
+				size = (coord[2]+abs(center_diff[0]), coord[3]+abs(center_diff[1]))
+				#calculate blitting position
+				pos = (max(0, center_diff[0]), max(0, center_diff[1]))
+				surf = pygame.Surface(size, SRCALPHA) #create new surface
+				surf.blit(image, pos, coord) #blit proper section of image
+			self.part_images[part_image.getAttribute("id")] = surf #store created image
+		self.layout = PartAnimationGroup(self, g, anim_dom.getElementsByTagName("layout")[0]) #create layout
+			
