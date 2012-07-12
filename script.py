5cd05addd8d983e8ba0231632763d5cdff2bee8b
Thomas <twatson52@mac.com>
2012-07-11 19:51:36 -0500

    added ability to set object visibility

--- script.py
+++ script.py
@@ -29,15 +29,18 @@ class Script:
 		except:
 			pass
 		return None #we couldn't parse it
+	def get_object(self, name): #get a certain object
+		if name == "" or name == "self": #if it's a blank name or refers to self
+			return self.obj #return the object we're bound to
+		elif name == "none": #if it's none
+			return None
+		else:
+			return self.obj.game.objects[name] #return the 
 	def dialog_cb(self, result): #callback for dialog completion
 		self.vars["dlog_result"] = result #store result in variables
 	def cmd_dialog(self, cmd): #handle command
 		self.dlog_wait = True #we're waiting for a dialog
-		if cmd.getAttribute("talker") != "": #if somebody else is supposed to be talking
-			obj = self.obj.game.objects[cmd.getAttribute("talker")] #get their object
-		else:
-			obj = self.obj #if not, use the one we're attached to
-		self.obj.game.show_dlog(data.get_node_text(cmd), obj, callback=self.dialog_cb) #show the dialog
+		self.obj.game.show_dlog(data.get_node_text(cmd), self.get_object(cmd.getAttribute("talker")), callback=self.dialog_cb) #show the dialog
 	def cmd_if(self, cmd): #handle if command
 		#get parameters
 		left = self.get_var(cmd.getAttribute("left"))
@@ -57,6 +60,13 @@ class Script:
 				return
 			self.callstack.append(self.curr_command.nextSibling) #save next command
 			self.curr_command = next[0].firstChild #store first command of else
+	def cmd_set_visible(self, cmd): #handle set visible command
+		to = cmd.getAttribute("to").lower() #calculate new visibility
+		if to == "true":
+			to = True
+		else:
+			to = False
+		self.get_object(cmd.getAttribute("what")).visible = to #set visibility
 	def next_cmd(self): #process the next command
 		if not self.running: return True #return if we aren't running
 		#return if we're waiting for a dialog and one is being shown
@@ -72,6 +82,8 @@ class Script:
 		elif self.curr_command.localName == "if": #if it's an if
 			self.cmd_if(self.curr_command) #handle it
 			return #skip going to next command
+		elif self.curr_command.localName == "set_visible": #change an object's visibility
+			self.cmd_set_visible(self.curr_command)
 		self.curr_command = self.curr_command.nextSibling #go to next command
 	def update(self): #update script state
 		if not self.running: return #return if we aren't running
