603bf56c52e1ca641cfb85c8526bf7c86a152518
Thomas <twatson52@mac.com>
2012-07-13 10:19:11 -0500

    added display of player's pokemon

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 import dialog #load dialog manager
 import settings #and game settings
+import font
 
 class Menu: #class to manage the in-game menus
 	def __init__(self, game):
@@ -14,10 +15,11 @@ class Menu: #class to manage the in-game menus
 		self.start_main_update() #start displaying main selection
 		self.show = self.start_main_update #set function to show ourselves
 	def start_main_update(self): #start displaying main selection
-		self.choices = ["Save", "Title Screen", "Cancel"] #store currently shown choices
+		self.choices = ["Pok{ae}mon", "Save", "Title Screen", "Cancel"] #store currently shown choices
 		self.choice_dlog.show_choices(self.choices) #start choice dialog showing
 		self.update_func = self.main_update #store our update function
 	def main_update(self, surf): #display main selection
+		self.menu = None
 		choice = self.choice_dlog.update(surf, (0, 0)) #draw selection
 		if choice is None: #if a choice hasn't been picked yet
 			return #don't do anything
@@ -27,6 +29,8 @@ class Menu: #class to manage the in-game menus
 			self.start_title_screen() #start showing info
 		elif self.choices[choice] == "Save": #if save was pressed
 			self.start_save() #start showing info
+		elif self.choices[choice] == "Pok{ae}mon": #if pokemon was pressed
+			self.start_pokemon() #start showing info
 	def start_title_screen(self): #start showing title screen questions
 		#show question
 		self.dlog.draw_text("Would you like to return to the title screen?{choices}Yes{endchoice}No{endchoice}{endchoices}")
@@ -54,5 +58,28 @@ class Menu: #class to manage the in-game menus
 			self.dlog.draw_text("Saving...") #show saving message
 		elif choice == 1: #if the user said not to save
 			self.game.menu_showing = False #menu shouldn't be showing any more
+	def start_pokemon(self):
+		self.menu = PokemonMenu(self) #create pokemon menu
+		self.update_func = self.menu.update #start its update function
 	def update(self, surf): #update ourselves
 		self.update_func(surf) #call current update function
+
+class PokemonMenu: #menu for displaying pokemon
+	def __init__(self, menu):
+		self.game = menu.game #store parameters
+		self.g = menu.g
+		self.menu = menu
+		#initialize stuff
+		self.font = font.Font("fonts/dialog_font.xml") #create font for drawing
+	def finish(self):
+		self.menu.show() #re-show the menu
+	def update(self, surf): #update ourselves
+		surf.fill((255, 255, 255)) #clear surface
+		ypos = 10
+		for mon in self.game.player.pokemon: #loop through player's pokemon
+			self.font.render(mon.show_name, surf, (10, ypos)) #show the pokemon's name
+			ypos += self.font.height
+			self.font.render("HP: "+str(mon.hp)+"/"+str(mon.stats[0])+" Lv: "+str(mon.level), surf, (10, ypos))
+			ypos += self.font.height+4
+		if self.g.curr_keys[settings.key_accept]: #if accept has been pressed
+			self.finish() #we're done
